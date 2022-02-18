98bd2e09734b6576df175010acb7a29e6fa11207
LouisMRC <36147361+LouisMRC@users.noreply.github.com>
2022-02-18 20:51:09 +0100

    project setup

---
+++ window.hpp
@@ -0,0 +1,18 @@
+#ifndef WINDOW_HPP
+#define WINDOW_HPP
+
+#include <SFML/Graphics.hpp>
+
+class Window
+{
+public:
+    Window();
+
+    void repaint(sf::RenderTarget *panel, sf::Color bgColor);
+
+    static sf::Color getColor(int soundAmp);
+protected:
+    sf::RenderWindow *win;
+};
+
+#endif // WINDOW_HPP
